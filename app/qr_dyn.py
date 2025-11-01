a0bf4ad3c38444ff4ee95846d66c2ba00e1d4798
Yabix <156900175+Ycmmn@users.noreply.github.com>
2025-11-01 12:13:30 +0000

    add app/qr_dyn.py

--- app/qr_dyn.py
+++ app/qr_dyn.py
@@ -9,7 +9,10 @@ import socket
 import concurrent.futures
 import time
 from pathlib import Path
-from pathlib import Path
 from pdf2image import convert_from_path
 from PIL import Image
 from typing import Union, List, Dict, Any
+from urllib.parse import urlparse, unquote
+import warnings, ctypes, os
+warnings.filterwarnings("ignore")
+os.environ["ZBAR_LOG_LEVEL"] = "0"
