f2da5d618d28656d256f6233b25e27f398e4e333
Yabix <156900175+Ycmmn@users.noreply.github.com>
2025-11-01 12:49:41 +0000

    add app/mix_ocr_qr.py

--- app/mix_ocr_qr.py
+++ app/mix_ocr_qr.py
@@ -8,3 +8,7 @@ import json
 SESSION_DIR = Path(os.getenv("SESSION_DIR", Path.cwd()))
 SOURCE_FOLDER = Path(os.getenv("SOURCE_FOLDER", SESSION_DIR / "uploads"))
 RENAMED_DIR = Path(os.getenv("RENAMED_DIR", SESSION_DIR / "renamed"))
+
+OCR_FILE = Path(os.getenv("OCR_FILE", SESSION_DIR / "gemini_output.json"))
+QR_FILE = Path(os.getenv("QR_FILE", SESSION_DIR / "final_superqr_v6_clean.json"))
+OUTPUT_FILE = Path(os.getenv("OUTPUT_FILE", SESSION_DIR / "mix_ocr_qr.json"))
